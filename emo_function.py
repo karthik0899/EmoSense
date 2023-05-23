43b6ebd5b471aa8d905b533f9cc1286dd6366110
Swarnava Bhattacharjee <92724359+Iam-007Swarna@users.noreply.github.com>
2023-05-24 00:14:10 +0530

    Update emo_function.py
    Added Functions:
    -------pre-processing-------
    original_split
    filter_3_0
    get_3_0
    add_bins_to_dataset
    ------------------visualization----------------
    bins_distribution

--- emo_function.py
+++ emo_function.py
@@ -177,6 +177,116 @@ def preprocess_dataframe(df):
     
     return df
 
+def get_3_0(df):
+    """
+    Filters a DataFrame to retrieve rows where V, A, and D values are all equal to 3.0.
+
+    Args:
+        df (pandas.DataFrame): The input DataFrame to filter.
+
+    Returns:
+        pandas.DataFrame: A new DataFrame containing only rows where V, A, and D are all 3.0.
+    """
+    # Filter rows where V, A, and D values are all equal to 3.0
+    VAD_3_0 = df[(df.V == 3.0) & (df.A == 3.0) & (df.D == 3.0)]
+    
+    # Reset the index of the filtered DataFrame
+    VAD_3_0.reset_index(drop=True, inplace=True)
+    
+    # Return the filtered DataFrame
+    return VAD_3_0
+
+def filter_3_0(df):
+    """
+    Filters a DataFrame to remove rows where V, A, and D values are all equal to 3.0.
+
+    Args:
+        df (pandas.DataFrame): The input DataFrame to filter.
+
+    Returns:
+        pandas.DataFrame: A new DataFrame with rows removed where V, A, and D are all 3.0.
+    """
+    # Filter rows where V, A, or D values are not equal to 3.0
+    VAD_filtered = df[(df['V'] != 3.0) | (df['A'] != 3.0) | (df['D'] != 3.0)]
+    
+    # Reset the index of the filtered DataFrame
+    VAD_filtered.reset_index(drop=True, inplace=True)
+    
+    # Return the filtered DataFrame
+    return VAD_filtered
+
+def add_bins_to_dataset(dataset, inplace=False):
+    """
+    Adds binning columns to the dataset based on the presence of 'V', 'A', and 'D' columns.
+
+    This function takes a dataset and adds binning columns for the 'V', 'A', and 'D' columns. 
+    Binning is a process of dividing continuous values into discrete intervals (bins). The binning 
+    columns are added to the dataset to represent the binned values of the original columns.
+
+    Args:
+        dataset (pandas.DataFrame): The dataset to modify.
+        inplace (bool, optional): If True, modifies the dataset in-place. If False, creates a new copy 
+            of the dataset. Defaults to False.
+
+    Returns:
+        pandas.DataFrame: The modified dataset with the added binning columns.
+
+    Notes:
+        The binning process uses the following interval ranges:
+        - Bin 0: (1.0, 2.0]
+        - Bin 1: (2.0, 3.0]
+        - Bin 2: (3.0, 4.0]
+        - Bin 3: (4.0, 5.0]
+
+    """
+
+    if not inplace:
+        dataset = dataset.copy()
+
+    # Define the columns to check for
+    columns = ['V', 'A', 'D']
+
+    # Check for missing columns
+    missing_columns = [col for col in columns if col not in dataset.columns]
+
+    for col in missing_columns:
+        print(f"Column '{col}' is missing 😥.")
+
+    # Define the bin edges
+    bins = [1, 2, 3, 4, 5]
+
+    # Apply binning to the available columns
+    if 'V' in dataset.columns:
+        dataset['V_bins'] = pd.cut(dataset['V'], bins=bins, labels=False, include_lowest=True)
+    if 'A' in dataset.columns:
+        dataset['A_bins'] = pd.cut(dataset['A'], bins=bins, labels=False, include_lowest=True)
+    if 'D' in dataset.columns:
+        dataset['D_bins'] = pd.cut(dataset['D'], bins=bins, labels=False, include_lowest=True)
+
+    # Return the modified dataset
+    return dataset
+
+def original_split(df):
+    """
+    Splits a dataset into three subsets: train, dev, and test.
+
+    This function takes a dataset and splits it into three subsets based on the 'split' column. The 'split' column
+    is expected to contain the values 'train', 'dev', or 'test' to indicate the subset membership of each row.
+
+    Args:
+        df (pandas.DataFrame): The dataset to split.
+
+    Returns:
+        tuple: A tuple containing the train, dev, and test subsets as separate pandas.DataFrames.
+
+    Notes:
+        This splitting approach is based on the suggestion by the original EmoBank team.
+
+    """
+    train_df = df[df['split'] == 'train'].copy()
+    dev_df = df[df['split'] == 'dev'].copy()
+    test_df = df[df['split'] == 'test'].copy()
+    return train_df, dev_df, test_df
 
 #-------------------------------------------------------------------Data EDA & Analysis ---------------------------------------------------------------------------------
 
@@ -642,4 +752,73 @@ def plot_word_cloud(data_frame, column_name, width=1500, height=1000, max_words=
     plt.axis("off")
     plt.tight_layout(pad=0)
     plt.show()
+    return plt.show()
+
+def bins_distribution(df):
+    """
+    Plots histograms and max count labels for specified columns in the given DataFrame.
+
+    Args:
+        df (pandas.DataFrame): Input DataFrame containing the data.
+
+    Returns:
+        matplotlib.figure.Figure: The resulting figure containing the histograms.
+
+    Note:
+        The function checks if the specified columns ('V_bins', 'A_bins', 'D_bins') are present in the DataFrame.
+        If a column is not present, it will print a message indicating that the column is not available.
+        The figure will be returned only for available columns, and there won't be any blank plots.
+
+    """
+    # Determine available columns
+    available_columns = []
+    for column in ['V_bins', 'A_bins', 'D_bins']:
+        if column in df.columns:
+            available_columns.append(column)
+        else:
+            print(f"Column '{column}' is not available 😥.")
+
+    # Count the number of available columns
+    num_plots = len(available_columns)
+
+    if num_plots == 0:
+        print("No available columns to plot 😭.")
+        return None
+
+    # Determine the number of rows and columns for subplots based on available columns
+    nrows = 1
+    ncols = num_plots
+
+    # Create a figure with subplots
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(4 * ncols, 4))
+
+    # Define the x-labels
+    x_labels = ['1-2', '2-3', '3-4', '4-5']
+
+    # Iterate over available columns and plot histograms
+    for i, column in enumerate(available_columns):
+        counts = df[column].value_counts().sort_index()
+
+        # Set a fixed range for the x-axis
+        x_range = np.arange(len(x_labels))
+
+        # Add zero counts for missing bins
+        counts = counts.reindex(x_range, fill_value=0)
+
+        ax = axes[i] if num_plots > 1 else axes  # Handle subplot indexing if only one subplot
+        ax.bar(counts.index, counts, align='center', edgecolor='black')
+
+        for j, count in enumerate(counts):
+            ax.text(j, count, f'Count: {count} \n {np.round((count/len(df[column]))*100,2)}%', ha='center', va='bottom', color='red')
+
+        ax.set_xticks(x_range)
+        ax.set_xticklabels(x_labels)
+        ax.set_xlabel('Bins')
+        ax.set_ylabel('Frequency')
+        ax.set_title(f'{column} Distribution', pad=25)
+
+    # Adjust the spacing between subplots
+    plt.tight_layout()
+
+    # Return the figure
     return plt.show()
